ffda065e7684c78242a2868dd6d2e6724e4bb91f
bagusadhy <bagus.adhy182@gmail.com>
2021-12-21 11:34:56 +0700

    finish migration Camp, Camp_benefit, Checkout

---
+++ database/migrations/2021_12_21_033002_create_camp_benefits_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            //    basic way
+            // $table->bigInteger('camp_id')->unsigned();
+            // $table->unsignedBigInteger('camp_id');
+
+            // simple way
+            $table->foreignId('camp_id')->constrained();
+
+            $table->string('name');
+            $table->timestamps();
+
+            // basic way
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
