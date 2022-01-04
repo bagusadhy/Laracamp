5b07fe57c192883cfc287d2a700dae4c301c4517
bagusadhy <bagus.adhy182@gmail.com>
2022-01-04 08:41:04 +0700

    fixing midtrans snap redirect and callback

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -70,6 +70,8 @@ class CheckoutController extends Controller
         $user->email = $data['email'];
         $user->name = $data['name'];
         $user->occupation = $data['occupation'];
+        $user->phone = $data['phone'];
+        $user->address = $data['address'];
         $user->save();
 
         // add data to checkout
@@ -135,7 +137,7 @@ class CheckoutController extends Controller
     /**
      * Midtrans Handler
      */
-    public function getSnapRedirect(Checkout $chekcout)
+    public function getSnapRedirect(Checkout $checkout)
     {
         $orderId = $checkout->id.'-'.Str::random(5);
         $price = $checkout->Camp->price * 1000;
@@ -150,6 +152,7 @@ class CheckoutController extends Controller
         $item_details[] = [
             'id' => $orderId,
             'price' => $price,
+            'quantity' => 1,
             'name' => "Payment for {$checkout->Camp->title} Camp"
         ];
 
@@ -179,7 +182,7 @@ class CheckoutController extends Controller
         ];
 
         try {
-          $paymentUrl = \Midtrans\Snap::createTransaction($param)->redirect_url();
+          $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
           $checkout->midtrans_url = $paymentUrl;
           $checkout->save();
 
@@ -192,7 +195,7 @@ class CheckoutController extends Controller
 
     public function midtransCallback(Request $request)
     {
-        $notif = new Midtrans\Notification();
+        $notif = $request->method() == 'POST' ? new Midtrans\Notification() : Midtrans\Transaction::status($request->order_id);
 
         $transaction_status = $notif->transaction_status;
         $fraud = $notif->fraud_status;
